c1f89eb455f90435dd396a6ecd253e152f7fe9ac
Crystal Martin <drcrystalm@gmail.com>
2019-05-23 22:33:44 -0500

    tried working on HeaderTitle, same thing

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return ( <
+        div >
+        <
+        h3 class name = "HeaderTitle" > Lambda School < /h3> <
+        /div>
+    )
+};
+
+
+export default HeaderTitle
