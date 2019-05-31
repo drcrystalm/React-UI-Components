0747ec9fb53733a33df0bbff44fbc63c97553716
ckopecky <kopecky12112@gmail.com>
2019-05-31 16:16:36 -0700

    change casing on import

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,8 +1,9 @@
 import React from "react"
 import "./App.css"
-import HeaderContainer from "./Components/HeaderComponents/HeaderContainer"
-import CardContainer from "./Components/CardComponents/CardContainer"
-import Footer from "./Components/FooterComponents/Footer"
+import HeaderContainer from "./components/HeaderComponents/HeaderContainer"
+import CardContainer from "./components/CardComponents/CardContainer"
+import Footer from "./components/FooterComponents/Footer"
+// make sure the path names match exactly what is in your file. Casing might not matter on Windows, but it matters on Mac, so I had to correct these to compile your project. You will most likely work in a Mac environment, so I would get used to the convention of casing matters. 
 
 const App = () => {
     return (
