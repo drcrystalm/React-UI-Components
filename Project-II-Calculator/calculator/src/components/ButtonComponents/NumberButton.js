c333b35a21d0442d43dfdd5a08e49986473f1ebc
Crystal Martin <drcrystalm@gmail.com>
2019-06-09 22:25:36 -0500

    started on top 0 display

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,20 @@
-import React from 'react';
-import './Button.css';
+import React from "react"
+import "./Button.css"
+
+const NumberButton = () => {
+    return (
+        <div>
+            <div>
+                <h1>7</h1>
+            </div>
+            <div>
+                <h1>8</h1>
+            </div>
+            <div>
+                <h1>9</h1>
+            </div>
+        </div>
+    )
+}
+
+export default NumberButton
